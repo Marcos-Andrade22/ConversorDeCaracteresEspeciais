6eaf113e5436a963b5b952971cedabc34e9fcfce
Marcos Andrade <mvpdeandrade@gmail.com>
2023-05-17 23:12:22 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorDeCaracteresEspeciais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ConversorDeCaracteresEspeciais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine("Digite um texto:");
+            string texto = Console.ReadLine();
+            sb.Append(texto);
+            string textoConvertido = ConverterSetaEmDiante(sb);
+
+            Console.WriteLine("Texto convertido:");
+            Console.WriteLine(textoConvertido);
+        }
+
+        static string ConverterSetaEmDiante(StringBuilder sb)
+        {
+            string textoConvertido = sb.ToString().Replace("//", "\\//");
+            textoConvertido = sb.ToString().Replace("\\//", "-");
+            textoConvertido = sb.ToString().Replace("-->" , "em diante");
+            return textoConvertido;
+        }
+    }
+}
